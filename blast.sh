9c5eb7bd598d7d9574743fa5e4f2d206d91d9b23
lasyapenumarthi <89758944+lasyapen@users.noreply.github.com>
2026-02-09 13:42:06 -0500

    Update blast.sh

--- blast.sh
+++ blast.sh
@@ -2,7 +2,7 @@ ml BLAST+
 
 makeblastdb -in VFDB_setA_nt.fas -dbtype nucl -out vfdb_nucleotide_db
 
-QUERY="/scratch/lp27068/Olivia1_Data_Reads/virulence_analysis/concatenated_query.fasta"
+QUERY="virulence_analysis/concatenated_query.fasta"
 DB="vfdb_nucleotide_db"
 OUT_TAB="vfdb_blast_results_tabular.txt"
 OUT_STD="vfdb_blast_results_standard.txt"
@@ -10,6 +10,6 @@ OUT_STD="vfdb_blast_results_standard.txt"
 blastn -query $QUERY -db $DB -evalue 1e-5 -num_threads 4 -out $OUT_TAB -outfmt 6
 blastn -query $QUERY -db $DB -evalue 1e-5 -num_threads 4 -out $OUT_STD -outfmt 0
 
-#convert vfdb_blast_results_tabular.tx into a file called "annotated_blast_result.txt" that contains the full MAG name as column 1 (including the k141 part of the header)
+#convert vfdb_blast_results_tabular.txt into a file called "annotated_blast_result.txt" that contains the full MAG name as column 1 (including the k141 part of the header)
 
 
